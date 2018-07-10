9f14c16e99161f1542d19e9798e4f5604196a79f
Theodore Chuang <chuangtheodore@gmail.com>
2018-07-10 21:36:21 +1000

    Expanded .displayTodoos to display todoText value and toggle complete

--- vanillaTodos.js
+++ vanillaTodos.js
@@ -1,25 +1,36 @@
 let todoList = {
     todos: [],
-    displayTodos: function() {
-        console.log('My Todos', this.todos);
+    displayTodos: function () {
+        if (this.todos.length === 0) {
+            console.log("Your todo list is empty! Let's go!")
+        } else {
+            console.log('My Todos: ');
+            for (i = 0; i < this.todos.length; i++) {
+                if (this.todos[i].completed === true) {
+                    console.log('(X) ', this.todos[i].todoText);
+                } else {
+                    console.log('( ) ', this.todos[i].todoText);
+                }
+            }
+        }
     },
-    addTodo: function(todoText) {
+    addTodo: function (todoText) {
         this.todos.push({
             todoText: todoText,
             completed: false
         })
         this.displayTodos();
     },
-    changeTodo: function(position, todoText) {
+    changeTodo: function (position, todoText) {
         this.todos[position].todoText = todoText;
         this.displayTodos();
     },
-    toggleCompleted: function(position) {
+    toggleCompleted: function (position) {
         let todo = this.todos[position];
         todo.completed = !todo.completed;
         this.displayTodos();
     },
-    deleteTodo: function(position) {
+    deleteTodo: function (position) {
         this.todos.splice(position, 1);
         this.displayTodos();
     }
